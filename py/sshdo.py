afccc49dc54d689414069b97c8c73d5f73c52dc1
whatacold <whatacold@gmail.com>
2017-04-04 23:59:37 +0800

    Make username/password optional.

--- py/sshdo.py
+++ py/sshdo.py
@@ -1,16 +1,19 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+'''Execute specified command on every hosts, which are specified in a file.
+'''
+
 import pexpect
-import sys
+import sys, os, getopt
 
 def usage():
-    sys.stderr.write('''Usage: {} <username> <password> <hosts-file> <cmd>
-    <hosts-file>    hosts list, one host per line
-'''.format(sys.argv[0]))
-    sys.exit(-1)
+    sys.stderr.write('''Usage: {} [-u <username>] [-p <password>] <hosts-file> <cmd>
 
-def do(user, passwd, hosts_file, cmd):
+    <hosts-file>    hosts list, one host per line\n'''.format(sys.argv[0]))
+    sys.exit(1)
+
+def do(hosts_file, cmd, user=None, passwd=None):
     hosts = []
     with open(hosts_file, 'r') as fp:
         hosts = fp.read().strip().split('\n')
@@ -18,32 +21,57 @@ def do(user, passwd, hosts_file, cmd):
     for host in hosts:
         try:
             print('>> Execute "{}" on {}'.format(cmd, host))
-            child = pexpect.spawn("ssh {}@{} '{}'".format(user, host, cmd))
+            if user:
+                child = pexpect.spawn("ssh {}@{} '{}'".format(user, host, cmd))
+            else:
+                child = pexpect.spawn("ssh {} '{}'".format(host, cmd))  # e.g. using private key
             #child.logfile = sys.stderr
 
             passwd_sent = False
-            i = child.expect(['password:', 'yes/no'], timeout=3)
-            if 0 == i:
-                child.sendline(passwd)
-                passwd_sent = True
-            elif 1 == i:
-                child.sendline('yes')
-
-            if not passwd_sent:
-                child.expect('password:', timeout=3)
-                child.sendline(passwd)
+            while not passwd_sent:
+                i = child.expect(['password:', 'yes/no'], timeout=3)
+                if 0 == i:
+                    if not passwd:
+                        sys.stderr.write('Password required but not supplied!\n')
+                        usage()
+                    child.sendline(passwd)
+                    passwd_sent = True
+                elif 1 == i:
+                    child.sendline('yes')
 
             child.interact()
-        except:
-            print('>> Failed on {}'.format(host))
+        except Exception as e:
+            print('>> Failed on {}: {}'.format(host, e))
 
 if '__main__' == __name__:
-    if 5 != len(sys.argv):
+    try:
+        opts, args = getopt.gnu_getopt(sys.argv[1:], 'u:p:')
+    except getopt.GetoptError as err:
+        print(err)
+        usage()
+
+    user = None
+    passwd = None
+    for o, a in opts:
+        if '-u' == o:
+            user = a
+        elif '-p' == o:
+            passwd = a
+        else:
+            usage()
+
+    if 2 != len(args):
+        usage()
+
+    hosts_file = args[0]
+    cmd = args[1]
+    if not os.path.isfile(hosts_file):
+        sys.stderr.write('No such file: {}\n'.format(hosts_file))
         usage()
 
     do(
-        sys.argv[1],
-        sys.argv[2],
-        sys.argv[3],
-        sys.argv[4]
+        hosts_file,
+        cmd,
+        user,
+        passwd
     )
